089f51152ee3187919dff85d141f6e6d57f48ae9
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-18 18:08:53 -0400

    Simplify USD formatting

--- app/shopping_mini.py
+++ app/shopping_mini.py
@@ -2,7 +2,10 @@
 from datetime import datetime
 import os
 
-checkout_at = datetime.now().strftime("%M/%d/%Y %I:%m %p")
+# GOAL 1: simplify USD formatting
+# GOAL 2: simplify the tax rate
+# GOAL 3: simplify receipt printing / file writing
+
 
 selected_products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -11,6 +14,20 @@ selected_products = [
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
 ] # FYI: for the purposes of this exercise, you won't need to modify this list at all
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    Param: my_price (int or float) like 4000.444444
+
+    Example: to_usd(4000.444444)
+
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+
+checkout_at = datetime.now().strftime("%M/%d/%Y %I:%m %p")
 
 now = datetime.now()
 
@@ -22,11 +39,11 @@ print("---------")
 print("CHECKOUT AT: " + str(now.strftime("%Y-%M-%d %H:%m:%S")))
 print("---------")
 for p in selected_products:
-    print("SELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+    print("SELECTED PRODUCT: " + p["name"] + "   " + to_usd(p["price"]))
 print("---------")
-print(f"SUBTOTAL: {subtotal:,.2f}")
-print(f"TAX: {(subtotal * 0.0875):.2f}")
-print(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}")
+print(f"SUBTOTAL: {to_usd(subtotal)}")
+print(f"TAX: {to_usd(subtotal * 0.0875)}")
+print(f"TOTAL: {to_usd((subtotal * 0.0875) + subtotal)}")
 print("---------")
 print("THANK YOU! PLEASE COME AGAIN SOON!")
 print("---------")
@@ -37,12 +54,12 @@ file_name = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{now.str
 with open(file_name, 'w') as f:
     f.write("------------------------------------------")
     for p in selected_products:
-        f.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+        f.write("\nSELECTED PRODUCT: " + p["name"] + "   " + to_usd(p["price"]))
 
     f.write("---------")
-    f.write(f"SUBTOTAL: {subtotal:,.2f}")
-    f.write(f"TAX: {(subtotal * 0.1):.2f}")
-    f.write(f"TOTAL: {((subtotal * 0.1) + subtotal):.2f}")
+    f.write(f"SUBTOTAL: {to_usd(subtotal)}")
+    f.write(f"TAX: {to_usd(subtotal * 0.1)}")
+    f.write(f"TOTAL: {to_usd((subtotal * 0.1) + subtotal)}")
     f.write("---------")
     f.write("THANK YOU! PLEASE COME AGAIN SOON!")
     f.write("---------")
